eaf46077c4a38cb3b2cdce01056ddf1b64f55c94
KeviN-XVII <kevinquartar@gmail.com>
2025-11-07 12:42:17 +0100

    Base progetto finita

--- backoffice.js
+++ backoffice.js
@@ -1,3 +1,5 @@
+const ApiUrl = "https://striveschool-api.herokuapp.com/api/product/";
+
 class Product {
   constructor(_name, _description, _brand, _price, _imageUrl) {
     this.name = _name;
@@ -9,6 +11,36 @@ class Product {
 }
 
 const form = document.getElementById("form-product");
+
+const url = location.search;
+const allP = new URLSearchParams(url);
+const id = allP.get("ProdId");
+if (id) {
+  fetch(ApiUrl + "/" + id, {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTBkYjU1NGY0YmQ0NzAwMTU4NWIxZTEiLCJpYXQiOjE3NjI1MDYwNjgsImV4cCI6MTc2MzcxNTY2OH0.bYGaU7MPXft1CZdq27sG1vLiqMgjLgQND7zM3zYnC4Q",
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error("Errore nel recupero del prodotto");
+      }
+    })
+    .then((product) => {
+      document.getElementById("name").value = product.name;
+      document.getElementById("description").value = product.description;
+      document.getElementById("brand").value = product.brand;
+      document.getElementById("price").value = product.price;
+      document.getElementById("imageUrl").value = product.imageUrl;
+    })
+    .catch((err) => {
+      console.log("Errore:", err);
+    });
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -21,10 +53,8 @@ form.addEventListener("submit", (e) => {
   const newProduct = new Product(name, description, brand, price, imageUrl);
   console.log("New Product Created:", newProduct);
 
-  const ApiUrl = "https://striveschool-api.herokuapp.com/api/product/";
-
-  fetch(ApiUrl, {
-    method: "POST",
+  fetch(ApiUrl + (id ? "/" + id : ""), {
+    method: id ? "PUT" : "POST",
     body: JSON.stringify(newProduct),
     headers: {
       Authorization:
@@ -34,7 +64,11 @@ form.addEventListener("submit", (e) => {
   })
     .then((res) => {
       if (res.ok) {
-        alert("Prodotto salvato con successo!");
+        alert(
+          id
+            ? "Prodotto modificato con successo"
+            : "Prodotto aggiunto con successo"
+        );
         form.reset();
       } else {
         alert("Errore non è stato aggiunto il prodotto");
--- details.js
+++ details.js
@@ -21,6 +21,11 @@ const getProductDetails = function () {
     })
     .then((product) => {
       console.log("product:", product);
+      document.getElementById("image").src = product.imageUrl;
+      document.getElementById("name").innerText = product.name;
+      document.getElementById("description").innerText = product.description;
+      document.getElementById("brand").innerText = product.brand;
+      document.getElementById("price").innerText = product.price + ",00€";
     })
     .catch((err) => {
       console.log("Errore:", err);
@@ -28,3 +33,28 @@ const getProductDetails = function () {
     });
 };
 getProductDetails();
+// bottone modifica
+const editEvent = function () {
+  location.assign(`./backoffice.html?ProdId=${id}`);
+};
+// bottone elimina
+const deleteEvent = function () {
+  fetch(ApiUrl + "/" + id, {
+    method: "DELETE",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTBkYjU1NGY0YmQ0NzAwMTU4NWIxZTEiLCJpYXQiOjE3NjI1MDYwNjgsImV4cCI6MTc2MzcxNTY2OH0.bYGaU7MPXft1CZdq27sG1vLiqMgjLgQND7zM3zYnC4Q",
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        alert("Prodotto eliminato con successo");
+        location.assign("homepage.html");
+      } else {
+        throw new Error("Errore nell'eliminazione del prodotto");
+      }
+    })
+    .catch((err) => {
+      console.log("Errore:", err);
+    });
+};
